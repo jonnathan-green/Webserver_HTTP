f41548ba8ddace3e714518f63d2bd7b211622c6b
unknown <jonnathan.rodriguez@cun.edu.co>
2022-09-12 20:53:38 -0500

    Prueba diferentes maneras de response, pero express es mucho mas completo

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-const http = require('http');
+const http = require('http')
 
-http.createServer( (req, res) => {
-    res.write('Hola jonnatnhan')
+http.createServer((req, res) =>{
+
+    //res.writeHead(200, {'Content-Type': 'application/json'})
+    res.setHeader('Content-Disposition','attachment; filename=lista.csv')
+    res.writeHead(200, {'Content-Type': 'application/csv'})
+
+    
+
+    res.write('id, nombre\n' )
+    res.write('1, Karen\n')
+    res.write('2, Camilo\n' )
+    res.write('3, Juan\n' )
     res.end();
 })
 
-.listen(8080)
+.listen( 8080 )
 
-console.log('Escuchando el puerto', 8080)
+console.log('Este puerto escucha el', 8080);
+
+
+//C:\Users\WINDOWS10\Documents\Jonnathan Green\Cursos\N ode.js\Seccion7_Webserver
