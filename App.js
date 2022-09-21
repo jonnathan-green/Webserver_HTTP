4432426c681faa4670861c4e0188ef6e055a54a8
unknown <jonnathan.rodriguez@cun.edu.co>
2022-09-20 22:31:37 -0500

    Mandando informacion del controlador a la vista

--- App.js
+++ App.js
@@ -7,7 +7,10 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home',{
+    nombre:' Jonnathan rodriguez',
+    titulo: 'Aprendiendo Node.js '
+  })
 })
 
 app.get('/generic', (req, res) => {
